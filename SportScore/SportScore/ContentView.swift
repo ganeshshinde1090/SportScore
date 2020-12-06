1695f59321df5733af15db32f8f2dd48a56ece94
Ganesh Shinde <ganesh.shinde@globant.com>
2020-12-06 13:25:54 +0530

    Added swift application.

---
+++ SportScore/SportScore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SportScore
+//
+//  Created by Ganesh Shinde on 06/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SportScore/SportScore/SportScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportScoreApp.swift
+//  SportScore
+//
+//  Created by Ganesh Shinde on 06/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SportScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
